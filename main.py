f0ae0f6b06ad45249c9a219d2319efa51b04b7af
Juan Montufar <juan.montufar@techworksgt.org>
2025-11-29 19:30:57 -0600

    feat: added retrieving access token for the corresponding installation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import jwt
 import httpx
 import hmac
 import hashlib
-from fastapi import FastAPI, HTTPException, Request, Header, status
+from fastapi import FastAPI, HTTPException, Request, Header
 from fastapi.responses import FileResponse
 from typing import List, Dict, Any
 
@@ -15,7 +15,7 @@ GITHUB_APP_ID = os.getenv("GITHUB_APP_ID")
 GITHUB_API_URL = os.getenv("GITHUB_API_URL", "https://api.github.com")
 GITHUB_PRIVATE_KEY_PATH = os.getenv("GITHUB_PRIVATE_KEY_PATH", "/app/certs/github.key")
 FAVICON_PATH = os.getenv("FAVICON_PATH", "favicon.ico")
-GITHUB_WEBHOOK_SECRET = os.getenv('GITHUB_WEBHOOK_SECRET') # New configuration
+GITHUB_WEBHOOK_SECRET = os.getenv("GITHUB_WEBHOOK_SECRET") # New configuration
 
 
 # --- Helper Functions ---
@@ -72,33 +72,7 @@ async def get_all_pages(client: httpx.AsyncClient, url: str, headers: Dict) -> L
     return results
 
 
-def verify_webhook_signature(payload_body: bytes, secret_token: str, signature_header: str):
-    """
-    Verifies that the webhook request came from GitHub.
-    """
-    if not signature_header:
-        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
-    
-    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
-    expected_signature = "sha256=" + hash_object.hexdigest()
-    
-    if not hmac.compare_digest(expected_signature, signature_header):
-        raise HTTPException(status_code=403, detail="Request signature is invalid")
-
-# --- Routes ---
-
-@app.get("/")
-async def root():
-    return {"message": "GitHub App Enterprise Reader (FastAPI). Go to /repos to list repositories."}
-
-
-@app.get("/favicon.ico", include_in_schema=False)
-async def favicon():
-    return FileResponse(FAVICON_PATH)
-
-
-@app.get("/repos")
-async def list_all_repos():
+async def get_repos_data(org_id = ""):
     """
     Async endpoint to fetch all repositories across all installations.
     """
@@ -125,9 +99,9 @@ async def list_all_repos():
             
             # 2. Loop through installations (Organizations)
             for install in installations:
-                print(install)
                 install_id = install["id"]
                 account_login = install["account"]["login"]
+                account_id = install["account"]["id"]
                 
                 # RE-GENERATE JWT (or reuse if valid) - for simplicity we reuse logic
                 # For installation token, we need a fresh request
@@ -135,40 +109,81 @@ async def list_all_repos():
                 # 3. Get Installation Access Token
                 token_url = f"{GITHUB_API_URL}/app/installations/{install_id}/access_tokens"
                 token_res = await client.post(token_url, headers=app_headers)
-                
+
+                # Ignore the accounts that return error                
                 if token_res.status_code != 201:
-                    all_repositories[account_login] = {"error": "Could not get token"}
+                    print(f"Error: Could not get token for account {account_login}")
                     continue
                     
                 access_token = token_res.json()["token"]
                 
-                # 4. List Repositories using the Token
-                repo_headers = {
-                    "Authorization": f"Bearer {access_token}",
-                    "Accept": "application/vnd.github+json"
+                # 4. Match the defined Org Name with its corresponsing installation
+                if org_id:
+                    # Return only the access token
+                    if org_id == account_id:
+                        return access_token
+
+                # Return all the repositories list
+                else:
+                    # 5. List Repositories using the Token
+                    repo_headers = {
+                        "Authorization": f"Bearer {access_token}",
+                        "Accept": "application/vnd.github+json"
+                    }
+                    
+                    # Endpoint to list repos available to this installation
+                    repos_url = f"{GITHUB_API_URL}/installation/repositories"
+                    
+                    # Fetch all pages asynchronously
+                    repos_data = await get_all_pages(client, repos_url, repo_headers)
+                    
+                    # Extract just names for cleaner output
+                    repo_names = [repo["full_name"] for repo in repos_data]
+                    all_repositories[account_login] = repo_names
+
+            if all_repositories:
+                return {
+                    "status": "success",
+                    "total_installations": len(installations),
+                    "data": all_repositories
                 }
-                
-                # Endpoint to list repos available to this installation
-                repos_url = f"{GITHUB_API_URL}/installation/repositories"
-                
-                # Fetch all pages asynchronously
-                repos_data = await get_all_pages(client, repos_url, repo_headers)
-                print(repos_data)
-                
-                # Extract just names for cleaner output
-                repo_names = [repo["full_name"] for repo in repos_data]
-                all_repositories[account_login] = repo_names
-                
-            return {
-                "status": "success",
-                "total_installations": len(installations),
-                "data": all_repositories
-            }
+            
+            return None
             
     except Exception as e:
         # catch-all for debugging
         raise HTTPException(status_code=500, detail=str(e))
 
+
+def verify_webhook_signature(payload_body: bytes, secret_token: str, signature_header: str):
+    """
+    Verifies that the webhook request came from GitHub.
+    """
+    if not signature_header:
+        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
+    
+    hash_object = hmac.new(secret_token.encode("utf-8"), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise HTTPException(status_code=403, detail="Request signature is invalid")
+
+# --- Routes ---
+
+@app.get("/")
+async def root():
+    return {"message": "GitHub App Enterprise Reader (FastAPI). Go to /repos to list repositories."}
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse(FAVICON_PATH)
+
+
+@app.get("/repos")
+async def all_repos():
+    repos_data = await get_repos_data()
+    return repos_data
 
 
 @app.post("/webhook")
@@ -178,7 +193,10 @@ async def webhook_handler(request: Request, x_hub_signature_256: str = Header(No
     Triggers when:
     1. A Pull Request is created (opened)
     2. A Repository is created
+    3. A Push is being executed
     """
+    data = None
+    msg = ""
     if not GITHUB_WEBHOOK_SECRET:
         raise HTTPException(status_code=500, detail="GITHUB_WEBHOOK_SECRET is not configured")
 
@@ -188,39 +206,35 @@ async def webhook_handler(request: Request, x_hub_signature_256: str = Header(No
 
     # 2. Parse Payload
     payload = await request.json()
-    action = payload.get('action')
+    action = payload.get("action")
     
     # 3. Handle Events based on X-GitHub-Event header
     
     # CASE A: Pull Request Opened
-    if x_github_event == 'pull_request':
-        if action == 'opened':
-            pr = payload['pull_request']
-            repo_name = payload['repository']['full_name']
-            pr_title = pr['title']
-            pr_user = pr['user']['login']
-            pr_url = pr['html_url']
-            
-            print(f"🚀 NEW PR DETECTED!")
-            print(f"Repo: {repo_name}")
-            print(f"User: {pr_user}")
-            print(f"Title: {pr_title}")
-            print(f"URL: {pr_url}")
-            
-            return {"status": "PR processed", "pr_title": pr_title}
+    if x_github_event == "pull_request":
+        if action == "opened":
+            data = payload["pull_request"]
+            msg = "PR processed"
 
     # CASE B: Repository Created
-    elif x_github_event == 'repository':
-        if action == 'created':
-            repo_name = payload['repository']['full_name']
-            owner_login = payload['repository']['owner']['login']
-            repo_url = payload['repository']['html_url']
+    elif x_github_event == "repository":
+        if action == "created":
+            data = payload["repository"]
+            msg = "Repository creation processed"
             
-            print(f"📦 NEW REPO CREATED!")
-            print(f"Repo: {repo_name}")
-            print(f"Owner: {owner_login}")
-            print(f"URL: {repo_url}")
-            
-            return {"status": "Repository creation processed", "repo_name": repo_name}
+    # CASE C: Pushed to a Repository
+    elif x_github_event == "push":
+        data = payload["push"]
+        msg = "Push event processed"
+
+    
+    if data:
+        repo_name = data["full_name"]
+        repo_url = data["html_url"]
+        org_id = data["owner"]["id"]
+        print(f"repo: {repo_name}. URL: {repo_url}. Org Id: {org_id}")
+        access_token = await get_repos_data(org_id)
+        return {"status": msg}
+        
 
     return {"status": f"Event '{x_github_event}' received but not processed"}
